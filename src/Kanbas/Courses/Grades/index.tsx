9fb6d81cc4838af98dd9789d73de752af29df907
joshiishaa <joshiishaa@gmail.com>
2024-02-22 19:58:27 -0500

    a3 v8

--- src/Kanbas/Courses/Grades/index.tsx
+++ src/Kanbas/Courses/Grades/index.tsx
@@ -1,6 +1,9 @@
 
 import { assignments, enrollments, grades, users } from "../../Database";
+import { FaCog, FaFilter } from "react-icons/fa";
 import { useParams } from "react-router-dom";
+import "./index.css";
+import "./style.css"
 function Grades() {
   const { courseId } = useParams();
   const as = assignments.filter((assignment) => assignment.course === courseId);
@@ -8,7 +11,38 @@ function Grades() {
   return (
     <div>
       <h1>Grades</h1>
-      <div className="table-responsive">
+      <div className = "container" > <button type="button">Import</button>
+                        <select>
+                            <option value="VAL1">Export</option>
+                         </select>
+                        <button type="button"><FaCog/></button></div>
+                           
+                    <br/>
+                 <form id="student-fields">
+                    <div className="row">
+                        <div className="col">
+                            <label ><h3>Student Names</h3></label>
+                        </div>
+                        <div className="col">
+                            <label><h3>Assignment Names</h3></label>
+                        </div>
+                      </div>
+                      <div className="row">
+                        <div className="col">
+                            <input className="form-control" id="student-name" placeholder="Search students"/>
+                        </div>
+                        <div className="col">
+                            <input className="form-control"  id="student-name" placeholder="Search assignments"/>
+                        </div>
+                      </div>
+                        
+                    
+                     </form>
+                     <br/>
+                     <button type="button"><FaFilter/>Apply Filters</button>
+                <br/>
+                <br/>
+      <div className="table-responsive table-striped">
         <table className="table">
           <thead>
             <th>Student Name</th>
